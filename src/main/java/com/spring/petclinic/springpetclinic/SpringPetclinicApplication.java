884b86f5d5248b516e325d31595073c6237b1371
Roxy <er.tajinder89@gmail.com>
2018-12-11 18:52:39 +0100

    project setup

---
+++ src/main/java/com/spring/petclinic/springpetclinic/SpringPetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.spring.petclinic.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetclinicApplication.class, args);
+	}
+}
