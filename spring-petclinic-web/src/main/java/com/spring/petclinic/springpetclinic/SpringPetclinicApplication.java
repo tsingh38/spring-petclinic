5cdccbb8b84e788f44c58f31ab6938736f98798a
Roxy <er.tajinder89@gmail.com>
2018-12-16 21:29:52 +0100

    index page and controller

--- spring-petclinic-web/src/main/java/com/spring/petclinic/springpetclinic/SpringPetclinicApplication.java
+++ spring-petclinic-web/src/main/java/com/spring/petclinic/springpetclinic/SpringPetclinicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPetclinicApplication {
 
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 		SpringApplication.run(SpringPetclinicApplication.class, args);
 	}
 }
