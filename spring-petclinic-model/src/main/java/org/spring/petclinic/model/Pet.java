78174101b84eec7302d6bcdc3fe922ff887eacaa
Roxy <er.tajinder89@gmail.com>
2018-12-23 17:46:31 +0100

    serializable added

--- spring-petclinic-model/src/main/java/org/spring/petclinic/model/Pet.java
+++ spring-petclinic-model/src/main/java/org/spring/petclinic/model/Pet.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 
 public class Pet extends BaseEntity{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private PetType petType;
 	private Owner owner;
 	private LocalDate birthDate;
