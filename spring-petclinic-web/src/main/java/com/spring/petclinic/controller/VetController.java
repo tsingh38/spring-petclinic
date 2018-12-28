01734d0bbbe79528bcfda4ea8fbd943f762c2b00
Roxy <er.tajinder89@gmail.com>
2018-12-28 19:09:00 +0100

    removed unused packagge

--- spring-petclinic-web/src/main/java/com/spring/petclinic/controller/VetController.java
+++ spring-petclinic-web/src/main/java/com/spring/petclinic/controller/VetController.java
@@ -4,8 +4,6 @@ package com.spring.petclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.spring.petclinic.services.OwnerService;
 import com.spring.petclinic.services.VetService;
 
 @Controller
